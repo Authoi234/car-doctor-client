012752053f9142e5c439380aa7786587517c2f75
Authoi234 <jawadkhan.authoi@gmail.com>
2023-11-25 00:31:01 +0630

    first commit

---
+++ src/Pages/Home/About/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import person from '../../../assets/images/about_us/person.jpg';
+import parts from '../../../assets/images/about_us/parts.jpg';
+
+const About = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className='w-1/2 '>
+                <img src="https://i.pinimg.com/originals/5f/25/39/5f253967e80d5e65add6d2d33c676655.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                </div>
+                <div className='w-1/2'>
+                    <p className="text-2xl text-orange-600 font-bold">About Us</p>
+                    <h1 className="text-5xl font-bold">We are qualified <br /> & of experience <br /> in this field</h1>
+                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <p className="py-6">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <button className="btn btn-primary">Get More Info</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;

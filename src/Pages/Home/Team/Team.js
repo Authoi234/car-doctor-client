6f623229c437d373ce72c3534534eac2445db102
Authoi234 <jawadkhan.authoi@gmail.com>
2023-11-30 23:07:55 +0630

    team cards are ready

--- src/Pages/Home/Team/Team.js
+++ src/Pages/Home/Team/Team.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import img1 from '../../../assets/images/team/1.jpg'
+import img2 from '../../../assets/images/team/2.jpg'
+import img3 from '../../../assets/images/team/3.jpg'
+import img4 from '../../../assets/images/team/4.jpg'
+import img5 from '../../../assets/images/team/5.jpg'
+import img6 from '../../../assets/images/team/6.jpg'
+import { FaFacebookF } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
 
 const Team = () => {
     return (
@@ -9,14 +19,17 @@ const Team = () => {
                 <p className='py-3 font-medium'>The majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </p>
             </div>
             <div className="carousel w-full">
-                <div id="cardSlide1" className="carousel-item relative min-w-fit">
+                <div id="cardSlide1" className="m-5 carousel-item relative min-w-fit">
                     <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                        <figure><img src={img1} alt="Shoes" /></figure>
+                        <div className="card-body p-3 text-center">
+                            <h2 className="text-3xl">Car Engine Plug</h2>
+                            <p className='text-xl text-slate-500'>Engine Expert</p>
+                            <div className="flex justify-center items-center">
+                                <FaFacebookF className='bg-gradient-to-r from-blue-500 to-blue-300 p-1 text-3xl mx-2 text-white rounded-full'></FaFacebookF>
+                                <FaTwitter className='bg-gradient-to-r from-cyan-500 to-blue-500 text-3xl p-1 mx-2 text-white rounded-full'></FaTwitter>
+                                <FaLinkedinIn className='bg-gradient-to-r from-blue-500 to-cyan-500 text-3xl p-1 mx-2 text-white rounded-full'></FaLinkedinIn>
+                                <FaInstagram className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-3xl mx-2 p-1 text-white rounded-full'></FaInstagram>
                             </div>
                         </div>
                     </div>
@@ -25,14 +38,17 @@ const Team = () => {
                         <a href="#cardSlide2" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="cardSlide2" className="carousel-item relative min-w-fit">
+                <div id="cardSlide2" className="m-5 carousel-item relative min-w-fit">
                     <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                        <figure><img src={img2} alt="Shoes" /></figure>
+                        <div className="card-body p-3 text-center">
+                            <h2 className="text-3xl">Car Engine Plug</h2>
+                            <p className='text-xl text-slate-500'>Engine Expert</p>
+                            <div className="flex justify-center items-center">
+                                <FaFacebookF className='bg-gradient-to-r from-blue-500 to-blue-300 p-1 text-3xl mx-2 text-white rounded-full'></FaFacebookF>
+                                <FaTwitter className='bg-gradient-to-r from-cyan-500 to-blue-500 text-3xl p-1 mx-2 text-white rounded-full'></FaTwitter>
+                                <FaLinkedinIn className='bg-gradient-to-r from-blue-500 to-cyan-500 text-3xl p-1 mx-2 text-white rounded-full'></FaLinkedinIn>
+                                <FaInstagram className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-3xl mx-2 p-1 text-white rounded-full'></FaInstagram>
                             </div>
                         </div>
                     </div>
@@ -41,14 +57,17 @@ const Team = () => {
                         <a href="#cardSlide3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="cardSlide3" className="carousel-item relative min-w-fit">
+                <div id="cardSlide3" className="m-5 carousel-item relative min-w-fit">
                     <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                        <figure><img src={img3} alt="Shoes" /></figure>
+                        <div className="card-body p-3 text-center">
+                            <h2 className="text-3xl">Car Engine Plug</h2>
+                            <p className='text-xl text-slate-500'>Engine Expert</p>
+                            <div className="flex justify-center items-center">
+                                <FaFacebookF className='bg-gradient-to-r from-blue-500 to-blue-300 p-1 text-3xl mx-2 text-white rounded-full'></FaFacebookF>
+                                <FaTwitter className='bg-gradient-to-r from-cyan-500 to-blue-500 text-3xl p-1 mx-2 text-white rounded-full'></FaTwitter>
+                                <FaLinkedinIn className='bg-gradient-to-r from-blue-500 to-cyan-500 text-3xl p-1 mx-2 text-white rounded-full'></FaLinkedinIn>
+                                <FaInstagram className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-3xl mx-2 p-1 text-white rounded-full'></FaInstagram>
                             </div>
                         </div>
                     </div>
@@ -57,14 +76,17 @@ const Team = () => {
                         <a href="#cardSlide4" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="cardSlide4" className="carousel-item relative min-w-fit">
+                <div id="cardSlide4" className="m-5 carousel-item relative min-w-fit">
                     <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                        <figure><img src={img4} alt="Shoes" /></figure>
+                        <div className="card-body p-3 text-center">
+                            <h2 className="text-3xl">Car Engine Plug</h2>
+                            <p className='text-xl text-slate-500'>Engine Expert</p>
+                            <div className="flex justify-center items-center">
+                                <FaFacebookF className='bg-gradient-to-r from-blue-500 to-blue-300 p-1 text-3xl mx-2 text-white rounded-full'></FaFacebookF>
+                                <FaTwitter className='bg-gradient-to-r from-cyan-500 to-blue-500 text-3xl p-1 mx-2 text-white rounded-full'></FaTwitter>
+                                <FaLinkedinIn className='bg-gradient-to-r from-blue-500 to-cyan-500 text-3xl p-1 mx-2 text-white rounded-full'></FaLinkedinIn>
+                                <FaInstagram className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-3xl mx-2 p-1 text-white rounded-full'></FaInstagram>
                             </div>
                         </div>
                     </div>
@@ -73,14 +95,17 @@ const Team = () => {
                         <a href="#cardSlide5" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="cardSlide5" className="carousel-item relative min-w-fit">
+                <div id="cardSlide5" className="m-5 carousel-item relative min-w-fit">
                     <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                        <figure><img src={img5} alt="Shoes" /></figure>
+                        <div className="card-body p-3 text-center">
+                            <h2 className="text-3xl">Car Engine Plug</h2>
+                            <p className='text-xl text-slate-500'>Engine Expert</p>
+                            <div className="flex justify-center items-center">
+                                <FaFacebookF className='bg-gradient-to-r from-blue-500 to-blue-300 p-1 text-3xl mx-2 text-white rounded-full'></FaFacebookF>
+                                <FaTwitter className='bg-gradient-to-r from-cyan-500 to-blue-500 text-3xl p-1 mx-2 text-white rounded-full'></FaTwitter>
+                                <FaLinkedinIn className='bg-gradient-to-r from-blue-500 to-cyan-500 text-3xl p-1 mx-2 text-white rounded-full'></FaLinkedinIn>
+                                <FaInstagram className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-3xl mx-2 p-1 text-white rounded-full'></FaInstagram>
                             </div>
                         </div>
                     </div>
@@ -89,14 +114,17 @@ const Team = () => {
                         <a href="#cardSlide6" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="cardSlide6" className="carousel-item relative min-w-fit">
+                <div id="cardSlide6" className="m-5 carousel-item relative min-w-fit">
                     <div className="card w-96 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                        <figure><img src={img6} alt="Shoes" /></figure>
+                        <div className="card-body p-3 text-center">
+                            <h2 className="text-3xl">Car Engine Plug</h2>
+                            <p className='text-xl text-slate-500'>Engine Expert</p>
+                            <div className="flex justify-center items-center">
+                                <FaFacebookF className='bg-gradient-to-r from-blue-500 to-blue-300 p-1 text-3xl mx-2 text-white rounded-full'></FaFacebookF>
+                                <FaTwitter className='bg-gradient-to-r from-cyan-500 to-blue-500 text-3xl p-1 mx-2 text-white rounded-full'></FaTwitter>
+                                <FaLinkedinIn className='bg-gradient-to-r from-blue-500 to-cyan-500 text-3xl p-1 mx-2 text-white rounded-full'></FaLinkedinIn>
+                                <FaInstagram className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-3xl mx-2 p-1 text-white rounded-full'></FaInstagram>
                             </div>
                         </div>
                     </div>

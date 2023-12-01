341249a154f03525a4675c0dc26723f4875653e6
Authoi234 <jawadkhan.authoi@gmail.com>
2023-12-01 23:44:18 +0630

    signpu pages outer design is ready

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import img from '../../assets/images/login/login.svg';
+import { Link } from "react-router-dom";
 
 const Signup = () => {
     return (
-        <div>
-            
+        <div className="hero w-full my-20">
+            <div className="hero-content grid md:grid-cols-2 gap-20 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img className="w-3/4" src={img} alt="" />
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20 text-center">
+                    <h1 className="text-5xl font-bold">Login</h1>
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input type="submit" value="Login" onClick={handleLogin} className="btn btn-primary" />
+                        </div>
+                    </form>
+                    <p className="font-bold text-center">
+                        New to Car Doctor <Link className="text-orange-600" to={'/signup'}>Signup</Link>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };

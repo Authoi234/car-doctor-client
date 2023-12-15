42429aaad36cc6ae6d296a6c40fa8975b53ed6ab
Authoi234 <jawadkhan.authoi@gmail.com>
2023-12-15 16:17:01 +0630

    error is gone and order row is ready

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,21 +1,46 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
+import OrderRow from './OrderRow';
 
 const Orders = () => {
-    const {user} = useContext(AuthContext);
-    const [orders, setOrders] = useState({});
+    const { user } = useContext(AuthContext);
+    const [orders, setOrders] = useState([]);
 
     // const url = `http://localhost:5000/orders?email=${user.email}`;
 
     useEffect(() => {
-        fetch(`http://localhost:5000/orders?email=${user.email}`)
-        .then(res => res.json())
-        .then(data => setOrders(data))
-    }, [])
+        fetch(`http://localhost:5000/orders?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => setOrders(data))
+    }, [user?.email])
 
     return (
         <div>
-            <h2 className="text-5xl font-bold">You Have {orders.length}</h2>
+            <h2 className="text-5xl font-bold">You Have {orders.length} Orders</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th>Message</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <OrderRow key={order._id} order={order}></OrderRow>)
+                        }                       
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
